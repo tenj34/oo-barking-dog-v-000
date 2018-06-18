d8234c79b052ea2f73a690014abc78d1036429cf
Juan Tenezaca <tenj34@gmail.com>
2018-06-18 00:26:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,19 @@
 class Dog
-  
 
-
-
-
+  def initialize(breed = "lab", name = "fido")
+    @breed = breed
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def name
+    return @name
+  end
+  def breed
+    return @breed
+  end
 end
